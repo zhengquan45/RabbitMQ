e0188079fbad13bcab071a288feb5b74071601a4
zhengquan <zheng_quan45@163.com>
2020-09-14 13:37:42 +0800

    SimpleMessageListenerContainer

--- rabbit-spring/src/main/java/org/zhq/spring/RabbitMQConfig.java
+++ rabbit-spring/src/main/java/org/zhq/spring/RabbitMQConfig.java
@@ -1,11 +1,17 @@
 package org.zhq.spring;
 
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.UUID;
 
 @Configuration
 @ComponentScan(basePackages = {"org.zhq.spring"})
@@ -26,5 +32,65 @@ public class RabbitMQConfig {
         RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
         rabbitAdmin.setAutoStartup(true);
         return rabbitAdmin;
+    }
+
+    @Bean
+    public Queue queue01() {
+        return new Queue("queue01", true, false, false, null);
+    }
+
+    @Bean
+    public Queue queue02() {
+        return new Queue("queue02", true, false, false, null);
+    }
+
+    @Bean
+    public Queue queue03() {
+        return new Queue("queue03", true, false, false, null);
+    }
+
+    @Bean
+    public Exchange exchange01() {
+        return new DirectExchange("exchange01", true, false, null);
+    }
+
+    @Bean
+    public Exchange exchange02() {
+        return new TopicExchange("exchange02", true, false, null);
+    }
+
+    @Bean
+    public Exchange exchange03() {
+        return new FanoutExchange("exchange03", true, false, null);
+    }
+
+    @Bean
+    public Binding binding01() {
+        return new Binding("queue01", Binding.DestinationType.QUEUE, "exchange01", "spring.add", null);
+    }
+
+    @Bean
+    public Binding binding02() {
+        return new Binding("queue02", Binding.DestinationType.QUEUE, "exchange02", "rabbit.#", null);
+    }
+
+    @Bean
+    public Binding binding03() {
+        return new Binding("queue03", Binding.DestinationType.QUEUE, "exchange03", "queue.#", null);
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer simpleMessageListenerContainer(ConnectionFactory connectionFactory) {
+        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer(connectionFactory);
+        simpleMessageListenerContainer.setQueues(queue01(), queue02(), queue03());
+        simpleMessageListenerContainer.setConcurrentConsumers(1);
+        simpleMessageListenerContainer.setMaxConcurrentConsumers(5);
+        simpleMessageListenerContainer.setDefaultRequeueRejected(false);
+        simpleMessageListenerContainer.setAcknowledgeMode(AcknowledgeMode.AUTO);
+        simpleMessageListenerContainer.setConsumerTagStrategy(queue -> queue + "-" + UUID.randomUUID().toString());
+        simpleMessageListenerContainer.setMessageListener((ChannelAwareMessageListener) (message, channel) -> {
+            System.out.println("consumer:" + new String(message.getBody()));
+        });
+        return simpleMessageListenerContainer;
     }
 }
--- rabbit-spring/src/test/java/org/zhq/spring/RabbitSpringApplicationTests.java
+++ rabbit-spring/src/test/java/org/zhq/spring/RabbitSpringApplicationTests.java
@@ -2,8 +2,10 @@ package org.zhq.spring;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -16,15 +18,18 @@ class RabbitSpringApplicationTests {
 
     @Autowired
     private RabbitAdmin rabbitAdmin;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     @Test
     void contextLoads() {
-        rabbitAdmin.declareExchange(new DirectExchange("test.direct",false,false));
-        rabbitAdmin.declareExchange(new TopicExchange("test.topic",false,false));
-        rabbitAdmin.declareExchange(new FanoutExchange("test.fanout",false,false));
+        rabbitAdmin.declareExchange(new DirectExchange("test.direct", false, false));
+        rabbitAdmin.declareExchange(new TopicExchange("test.topic", false, false));
+        rabbitAdmin.declareExchange(new FanoutExchange("test.fanout", false, false));
 
-        rabbitAdmin.declareQueue(new Queue("test.direct.queue",false));
-        rabbitAdmin.declareQueue(new Queue("test.topic.queue",false));
-        rabbitAdmin.declareQueue(new Queue("test.fanout.queue",false));
+        rabbitAdmin.declareQueue(new Queue("test.direct.queue", false));
+        rabbitAdmin.declareQueue(new Queue("test.topic.queue", false));
+        rabbitAdmin.declareQueue(new Queue("test.fanout.queue", false));
 
         rabbitAdmin.declareBinding(new Binding("test.direct.queue",
                 Binding.DestinationType.QUEUE,
@@ -34,14 +39,26 @@ class RabbitSpringApplicationTests {
 
         //可以直接用这种方式创建exchange 和 queue
         rabbitAdmin.declareBinding(BindingBuilder
-                .bind(new Queue("test.topic.queue",false))
-                .to(new TopicExchange("test.topic",false,false))
+                .bind(new Queue("test.topic.queue", false))
+                .to(new TopicExchange("test.topic", false, false))
                 .with("topic.#"));
         rabbitAdmin.declareBinding(BindingBuilder
-                .bind(new Queue("test.fanout.queue",false))
-                .to(new FanoutExchange("test.fanout",false,false)));
+                .bind(new Queue("test.fanout.queue", false))
+                .to(new FanoutExchange("test.fanout", false, false)));
 
         rabbitAdmin.purgeQueue("test.topic");
+    }
+
+    @Test
+    public void sendMessageTest() {
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.getHeaders().put("desc", "this is a description");
+        messageProperties.getHeaders().put("type", "this is a custom type");
+        Message message = new Message("this is a text".getBytes(), messageProperties);
+        rabbitTemplate.convertAndSend("exchange01","spring.add",message, msg -> {
+            msg.getMessageProperties().getHeaders().put("extend","this is a extend");
+            return msg;
+        });
     }
 
 }
