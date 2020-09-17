aabbc4f7072d465f8a19939521a3e1622c9facc5
zhengquan <zheng_quan45@163.com>
2020-09-17 14:57:17 +0800

    rabbit sample

--- rabbit-sample/src/main/java/org/zhq/SampleApplication.java
+++ rabbit-sample/src/main/java/org/zhq/SampleApplication.java
@@ -1,18 +1,24 @@
 package org.zhq;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zhq.rabbit.api.Message;
+import org.zhq.rabbit.api.MessageType;
 import org.zhq.rabbit.producer.broker.ProducerClient;
+
+import java.util.UUID;
 
 @SpringBootApplication
 @RestController
+@Slf4j
 public class SampleApplication {
     private final ProducerClient producerClient;
 
+    @Autowired
     public SampleApplication(ProducerClient producerClient) {
         this.producerClient = producerClient;
     }
@@ -20,13 +26,36 @@ public class SampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class, args);
     }
-//    rapidSend(Message message);
-//
-//    void confirmSend(Message message);
-//
-//    void reliantSend(Message message);
-    @GetMapping("/rapidSend")
-    public void rapidSend(){
 
+    @GetMapping("/rapidSend")
+    public void rapidSend() {
+        Message message = Message.builder().messageId(UUID.randomUUID().toString())
+                .messageType(MessageType.RAPID)
+                .topic("rapid-exchange")
+                .routingKey("rapid.add")
+                .build();
+        producerClient.send(message);
     }
+
+    @GetMapping("/confirmSend")
+    public void confirmSend() {
+        Message message = Message.builder().messageId(UUID.randomUUID().toString())
+                .messageType(MessageType.CONFIRM)
+                .topic("confirm-exchange")
+                .routingKey("confirm.111")
+                .build();
+        producerClient.send(message);
+    }
+
+
+    @GetMapping("/reliantSend")
+    public void reliantSend() {
+        Message message = Message.builder().messageId(UUID.randomUUID().toString())
+                .messageType(MessageType.RELIANT)
+                .topic("reliant-exchange")
+                .routingKey("reliant.bbbb")
+                .build();
+        producerClient.send(message);
+    }
+
 }
